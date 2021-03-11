2b88c0b75a31acb67b59e1f5c037059ec16a0e72
Tan Nguyen <nguyentannhat1103@gmail.com>
2021-03-11 01:09:04 -0600

    update 03/11/2021

--- js/check_length.js
+++ js/check_length.js
@@ -2,11 +2,7 @@ function checkLength(){
     //var user_name = document.getElementById("user_name");
     var user_name = document.forms["myForm"]["your-name"].value;
 
-    if (user_name.length<1) {
-        document.getElementById('error-name').innerHTML = " Please Enter Your Name *"
-
-    }
-    else if(user_name.length > 50){
+    if(user_name.length > 50){
         document.getElementById('error-name').innerHTML="User name is too long - exceed the maximum of 50 characters";
 
         //alert("User name is too long - exceed the maximum of 50 characters");
@@ -24,15 +20,33 @@ function checkLength(){
     }
 }
 
+function check_name() {
+    var first_name = document.getElementById("first_name");
+    var last_name = document.getElementById("last_name")
 
+    if(first_name && last_name) {
+        if (first_name.value.length > 50) {
+            document.getElementById("firstname").innerHTML = "First name is too long - exceed the maximum of 50 characters"
+        }
+        else {
+            document.getElementById("firstname").innerHTML = " ";
+        }
 
+        if (last_name.value.length > 50) {
+            document.getElementById("lastname").innerHTML = "Last name is too long - exceed the maximum of 50 characters"
+        }
+        else {
+            document.getElementById("lastname").innerHTML = " ";
+        }
+    }
 
-
-
+}
 
 
 function length(){
     document.getElementById("user_name").onblur = checkLength;
+    document.getElementById("first_name").onblur=check_name;
+    document.getElementById("last_name").onblur=check_name;
 }
 
 
